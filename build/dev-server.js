498247429e06bfdb50586f916ec37eb302ce85bf
xiarijs <404501527@qq.com>
2016-12-23 10:34:15 +0800

    step one

--- build/dev-server.js
+++ build/dev-server.js
@@ -17,6 +17,42 @@ var proxyTable = config.dev.proxyTable
 var app = express()
 var compiler = webpack(webpackConfig)
 
+// mock data
+var appData = require('../data.json');
+
+var seller = appData.seller;
+var goods = appData.goods;
+var ratings = appData.ratings;
+
+var apiRoutes = express.Router();
+
+apiRoutes.get('/seller',function(req , res){
+  res.json({
+    errno : 0,
+    data  : seller
+  });
+});
+
+apiRoutes.get('/goods',function(req , res){
+  res.json({
+    errno : 0,
+    data  : goods
+  });
+});
+
+apiRoutes.get('/ratings',function(req , res){
+  res.json({
+    errno : 0,
+    data  : ratings
+  })
+});
+
+app.use('/api',apiRoutes);
+
+
+
+
+
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
   publicPath: webpackConfig.output.publicPath,
   stats: {
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <div class="header">
+      I am header
+    </div>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
   </div>
 </template>
 
-<script>
-import Hello from './components/Hello'
-
-export default {
-  name: 'app',
-  components: {
-    Hello
-  }
-}
+<script type="text/ecmascript-6">
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style style="text/css">
+
 </style>
